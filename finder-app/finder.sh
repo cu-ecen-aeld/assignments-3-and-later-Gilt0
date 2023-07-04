006202d8c67a740ef032e5949790e1850eb90fac
Gilt0 <ga.coche@gmail.com>
2023-07-04 13:26:41 -0700

    working version

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -2,9 +2,20 @@
 
 if [ $# -lt 2 ]
 then
-    echo "Please run script using a command like: ./finder.sh <filesdir> <searchstr>"
+    echo "Please run script like this"
+    echo "./finder.sh <filesdir> <searchstr>"
+    echo "<filesdir>  is a directory"
+    echo "<searchstr> is a pattern to match in files of directory <filesdir>"
+    exit 1
+elif ! [ -d $1 ]
+then
+    echo "filesdir is not a directory"
     exit 1
 fi
 
-echo "Running with command: $0 $1 $2"
+X=$(find $1 -type f | wc -l)
+Y=$(find $1 -not -path '*/.*' -type f -exec grep -H "$2" {} \; | wc -l)
+
+echo "The number of files are $X and the number of matching lines are $Y" 
+
 exit 0
